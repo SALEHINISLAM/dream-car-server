29565771401a6b85157ccc7507622fbbb7ef86b2
SALEHIN <msionlinekingdom@gmail.com>
2024-08-15 21:55:09 +0600

    mongodb connected and id and pass hidden

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = 3000
+const cors=require('cors')
+require('dotenv').config()
 
-dream-car
-7q4FkQgB63BUnV8d
-7q4FkQgB63BUnV8d
-const uri = "mongodb+srv://<username>:<password>@cluster0.bdqfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const app = express()
+const port = process.env.PORT || 5001
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdqfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,7 +31,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
